6b08c34e304eb3f827ffce0a1047460b62f57319
Iraham Gardea <49107098+ChaosNeon@users.noreply.github.com>
2021-07-18 22:00:26 -0600

    Visual Overhaul

---
+++ scripts/prepo.js
@@ -0,0 +1,71 @@
+let label = document.getElementsByTagName("label")[0];
+let labelLives = document.getElementsByTagName("label")[2];
+let textArea = document.querySelector("textarea");
+let startButton = document.getElementsByClassName("btn")[0];
+let tryButton = document.getElementsByClassName("btn")[1];
+let nextButton = document.getElementsByClassName("btn")[2];
+let restartButton = document.getElementsByClassName("btn")[3];
+let lives;
+let winTextArea = document.getElementsByClassName("mb-3")[0];
+let answFeedback = document.createElement('p');
+
+
+
+let prepos = ['bis', 'für', 'ohne', 'durch', 'um', 'gegen', 'an', 'in', 'über', 'auf', 'unter', 'vor', 'hinter',
+'neben', 'zwischen', 'von', 'aus', 'nach', 'zu', 'mit', 'bei', 'ab', 'seit', 'ausserchen', 'gegenüber'];
+//----------------------------------------------------------------------------------------------------------------------//
+
+lives = 5;
+
+//Function to start the game
+function startGame() {
+    let numLet = Math.floor(Math.random() * 25);
+    if(prepos[numLet] === label.textContent){  //If the letter is the same, the function is called again
+        nextQuestion();
+    } else {
+        label.textContent = prepos[numLet];
+    }
+    labelLives.innerHTML = lives;
+    tryButton.removeAttribute("disabled");
+    startButton.setAttribute("disabled", '');
+    restartButton.removeAttribute("disabled");
+}
+
+//Checks if the answer is correct or incorrect
+function tryAnswer() {
+    let answ = textArea.value;
+    if(answ === answers[label.textContent]) {
+        answFeedback.innerHTML ="Noiceeeee";
+    } else {
+        
+        answFeedback.innerHTML = "Chavalo baboso";
+        lives--;
+       
+    }
+    winTextArea.appendChild(answFeedback);
+    nextButton.removeAttribute("disabled");
+    tryButton.setAttribute("disabled", '');
+    labelLives.innerHTML = lives;
+}
+
+//Changes the current letter to the next, without repetition
+function nextQuestion() {
+    let answ = textArea.value;
+    if(lives <= 0 && answ !== answers[label.textContent]) {
+        labelLives.innerHTML = "GAME OVER";
+        nextButton.setAttribute("disabled", '');
+        return;
+    }
+    startGame();
+    nextButton.setAttribute("disabled", '');
+    answFeedback.innerHTML = "";
+    textArea.value = "";
+}
+
+function restartGame() {
+    lives = 5;
+    labelLives.innerHTML = "---";
+    textArea.value = "";
+    startButton.removeAttribute("disabled");
+}
+//Añadir puntaje, botón de finalizar, boton de siguiente pregunta, vidas
